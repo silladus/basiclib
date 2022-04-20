366eeb00352495ca9058fe8bd5c138e916bbdef8
silladus <silladus@163.com>
2022-04-20 20:56:00 +0800

    popupwindow优化

--- basiclib/src/main/java/silladus/basic/PopupWindowConfig.java
+++ basiclib/src/main/java/silladus/basic/PopupWindowConfig.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.graphics.drawable.ColorDrawable;
-import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,7 +23,7 @@ import androidx.annotation.Nullable;
 
 public class PopupWindowConfig {
 
-    private PopupWindow mPopupWindow;
+    private final PopupWindow mPopupWindow;
     private int[] windowPos;
 
     @NonNull
@@ -52,65 +51,69 @@ public class PopupWindowConfig {
 
     private PopupWindowConfig(PopupWindow mPopupWindow) {
         this.mPopupWindow = mPopupWindow;
+
+        // 如果不设置PopupWindow的背景，有些版本就会出现一个问题：无论是点击外部区域还是Back键都无法dismiss弹框
+        mPopupWindow.setBackgroundDrawable(new ColorDrawable());
+        // 适应键盘输入
+        mPopupWindow.setInputMethodMode(PopupWindow.INPUT_METHOD_NEEDED);
+        mPopupWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
     }
 
     public void showAt(@NonNull View anchorView) {
         if (windowPos == null) {
-            Context context = anchorView.getContext();
-            Activity activity = getActivityFromContext(context);
-
-            if (activity == null) {
-                return;
-            }
-
-            // 如果不设置PopupWindow的背景，有些版本就会出现一个问题：无论是点击外部区域还是Back键都无法dismiss弹框
-            mPopupWindow.setBackgroundDrawable(new ColorDrawable());
             // 设置好参数之后再show
-            windowPos = calculatePopWindowPos(anchorView, mPopupWindow.getContentView());
-            final Window window = activity.getWindow();
-            //创建当前界面的一个参数对象
-            WindowManager.LayoutParams params = window.getAttributes();
-            params.alpha = alpha;
-            //把该参数对象设置进当前界面中
-            window.setAttributes(params);
-            mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
-                @Override
-                public void onDismiss() {
-                    WindowManager.LayoutParams params = window.getAttributes();
-                    //设置为不透明，即恢复原来的界面
-                    params.alpha = 1.0f;
-                    window.setAttributes(params);
-
-                    if (onDismissListener != null) {
-                        onDismissListener.onDismiss();
-                    }
-                }
-            });
+            windowPos = calculatePopWindowPos(anchorView, mPopupWindow.getContentView(), mPopupWindow.getWidth() - left + right);
         }
 
-        // 适应键盘输入
-        mPopupWindow.setInputMethodMode(PopupWindow.INPUT_METHOD_NEEDED);
-        mPopupWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        Context context = anchorView.getContext();
+        Activity activity = getActivityFromContext(context);
+
+        if (activity == null) {
+            return;
+        }
+
+        final Window window = activity.getWindow();
+        //创建当前界面的一个参数对象
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.alpha = alpha;
+        //把该参数对象设置进当前界面中
+        window.setAttributes(params);
+        mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                WindowManager.LayoutParams params = window.getAttributes();
+                //设置为不透明，即恢复原来的界面
+                params.alpha = 1.0f;
+                window.setAttributes(params);
+
+                if (onDismissListener != null) {
+                    onDismissListener.onDismiss();
+                }
+            }
+        });
+
+        int gravity = mGravity;
+        int x = 0;
+        int y = 0;
         if (mGravity != 0) {
             if (horizontal) {
-                mPopupWindow.showAtLocation(anchorView, mGravity, 0, windowPos[1]);
-            } else {
-                mPopupWindow.showAtLocation(anchorView, mGravity, 0, 0);
+                y = windowPos[1];
             }
         } else {
-            int[] pos = {Gravity.CENTER, Gravity.TOP | Gravity.START, Gravity.CENTER_HORIZONTAL, Gravity.BOTTOM | Gravity.START};
-            // 可以自己调整偏移
-            int offsetY = (int) TypedValue.applyDimension(
-                    TypedValue.COMPLEX_UNIT_DIP,
-                    mOffsetY,
-                    anchorView.getResources().getDisplayMetrics()
-            );
-            int y = windowPos[1] + offsetY;
-            mPopupWindow.showAtLocation(anchorView, pos[position], windowPos[0], y);
+            int[] pos = {
+                    Gravity.CENTER,
+                    Gravity.TOP | Gravity.START,
+                    Gravity.CENTER_HORIZONTAL,
+                    Gravity.BOTTOM | Gravity.START
+            };
+            gravity = pos[position];
+            x = windowPos[0];
+            y = windowPos[1] + top;
         }
+        mPopupWindow.showAtLocation(anchorView, gravity, x, y);
     }
 
-    private static int[] calculatePopWindowPos(@NonNull View anchorView, @NonNull View contentView) {
+    private static int[] calculatePopWindowPos(@NonNull View anchorView, @NonNull View contentView, int windowWidth) {
         int[] windowPos = new int[2];
         int[] anchorLoc = new int[2];
         // 获取锚点View在屏幕上的左上角坐标位置
@@ -119,11 +122,11 @@ public class PopupWindowConfig {
         // 获取屏幕的高宽
         int screenHeight = contentView.getResources().getDisplayMetrics().heightPixels;
         int screenWidth = contentView.getResources().getDisplayMetrics().widthPixels;
-        // 测量contentView
-        contentView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        // 计算contentView的高宽
+//        // 测量contentView
+//        contentView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+//        // 计算contentView的高宽
         int windowHeight = contentView.getMeasuredHeight();
-        int windowWidth = contentView.getMeasuredWidth();
+//        int windowWidth = contentView.getMeasuredWidth();
         // 判断需要向上弹出还是向下弹出显示
         boolean isNeedShowUp = screenHeight - anchorLoc[1] - anchorHeight < windowHeight;
         windowPos[0] = screenWidth - windowWidth;
@@ -147,21 +150,26 @@ public class PopupWindowConfig {
         return null;
     }
 
-    public PopupWindowConfig setOffsetY(int offsetY) {
-        this.mOffsetY = offsetY;
-        return this;
-    }
-
     public PopupWindowConfig setOnDismissListener(PopupWindow.OnDismissListener listener) {
         this.onDismissListener = listener;
         return this;
     }
 
     private PopupWindow.OnDismissListener onDismissListener;
-    private int mOffsetY = 80;
     private int mGravity;
     private int position = 1;
     private float alpha = 0.6f;
+
+    private int left;
+    private int top;
+    private int right;
+
+    public PopupWindowConfig setMargin(int left, int top, int right) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        return this;
+    }
 
     public PopupWindowConfig setGravityByIndex(int i) {
         position = i;
